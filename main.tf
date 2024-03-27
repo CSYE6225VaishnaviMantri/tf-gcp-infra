eb3bd057794b9dd64555fc11bb4c22df542ab7ed
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-03-27 14:42:18 -0400

    Assignment-7 Changes

--- main.tf
+++ main.tf
@@ -93,16 +93,18 @@ resource "google_compute_instance" "vm_instance" {
   tags = var.vm_tag
 
   metadata_startup_script = <<-EOT
-sudo bash -c 'cat > /tmp/application.properties' <<EOF
-spring.datasource.driver-class-name=com.mysql.jdbc.Driver
-spring.datasource.url=jdbc:mysql://${google_sql_database_instance.instance.private_ip_address}/${var.DB_NAME}?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=utf8
-spring.datasource.username=${var.DB_USER}
-spring.datasource.password=${random_password.db_user_password.result}
-spring.jpa.properties.hibernate.show_sql=true
-spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect
-spring.jpa.hibernate.ddl-auto=update
-EOF
-EOT
+  sudo bash -c 'cat > /tmp/application.properties' <<EOF
+  spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+  spring.datasource.url=jdbc:mysql://${google_sql_database_instance.instance.private_ip_address}/${var.DB_NAME}?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=utf8
+  spring.datasource.username=${var.DB_USER}
+  spring.datasource.password=${random_password.db_user_password.result}
+  spring.jpa.properties.hibernate.show_sql=true
+  spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect
+  spring.jpa.hibernate.ddl-auto=update
+  pubsub.projectId=${var.project_id}
+  pubsub.topicId=${google_pubsub_topic.pub_sub_topic.name}
+  EOF
+  EOT
 
 }
 
@@ -254,4 +256,116 @@ resource "google_project_iam_binding" "monitoring_metric_writer" {
   members = [
     "serviceAccount:${google_service_account.service_account.email}"
   ]
+}
+
+
+resource "google_pubsub_topic" "pub_sub_topic" {
+  name                       = var.pub_sub_name
+  message_retention_duration = var.pubsubtopic_message_retention_duration
+  depends_on                 = [google_service_networking_connection.private_services_connection]
+}
+
+# Create a Pub/Sub subscription
+resource "google_pubsub_subscription" "pub_sub_subscription" {
+  name                 = var.pubsub_subscription_name
+  topic                = google_pubsub_topic.pub_sub_topic.name
+  ack_deadline_seconds = var.ack_deadline_seconds
+  expiration_policy {
+    ttl = var.ttl
+  }
+}
+
+resource "google_service_account" "function_service_account" {
+  account_id   = var.cloudfunction_account_id
+  display_name = var.cloudfunction_display_name
+}
+
+resource "google_project_iam_binding" "function_service_account_roles" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+
+  members = [
+    "serviceAccount:${google_service_account.function_service_account.email}"
+  ]
+}
+
+resource "google_storage_bucket" "function_code_bucket" {
+  name     = var.cloudstorage_bucketname
+  location = var.region
+}
+
+resource "google_storage_bucket_object" "function_code_objects" {
+  name   = var.cloudstorage_bucketobjectname
+  bucket = google_storage_bucket.function_code_bucket.name
+  source = var.cloudstorage_source
+}
+
+resource "google_cloudfunctions2_function" "email_verification_function" {
+  name     = var.cloudfunction_name
+  location = var.region
+
+  build_config {
+    runtime     = var.cloudfunction_runtime
+    entry_point = var.cloudfunction_entry_point
+
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_code_bucket.name
+        object = google_storage_bucket_object.function_code_objects.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count            = var.max_instance_count
+    min_instance_count            = var.min_instance_count
+    available_memory              = var.available_memory
+    timeout_seconds               = var.timeout_seconds
+    vpc_connector                 = google_vpc_access_connector.connector.name
+    vpc_connector_egress_settings = var.vpc_connector_egress_settings
+
+    environment_variables = {
+      SQL_HOST           = google_sql_database_instance.instance.private_ip_address
+      SQL_USERNAME       = var.DB_USER
+      SQL_PASSWORD       = random_password.db_user_password.result
+      webappSQL_DATABASE = var.DB_NAME
+    }
+
+    ingress_settings               = var.ingress_settings
+    all_traffic_on_latest_revision = var.all_traffic_on_latest_revision
+    service_account_email          = google_service_account.function_service_account.email
+
+
+  }
+  event_trigger {
+    trigger_region = var.region
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.pub_sub_topic.id
+  }
+}
+
+
+resource "google_project_iam_binding" "pubsub_publisher" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+
+  members = [
+    "serviceAccount:${google_service_account.service_account.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "pubsub_service_account_roles" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+
+  members = [
+    "serviceAccount:${google_service_account.service_account.email}"
+  ]
+}
+
+resource "google_vpc_access_connector" "connector" {
+  name          = "serverless-vpc-connector"
+  region        = var.region
+  ip_cidr_range = "10.8.0.0/28"
+  network       = google_compute_network.vpcnetwork.name
 }
