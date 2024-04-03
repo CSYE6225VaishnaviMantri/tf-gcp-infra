4c0c7bc21da5fe20917e3360b87a04e38a99e1a2
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-04-03 17:16:37 -0400

    Updated variables of terraform

--- variablesofterraform.tf
+++ variablesofterraform.tf
@@ -269,4 +269,375 @@ variable "all_traffic_on_latest_revision" {
   type        = bool
 }
 
+variable "backend_service_name" {
+  description = "Name of the backend service"
+  default     = "backend-service"
+}
 
+
+variable "instance_group_name" {
+  description = "Name of the instance group"
+  default     = "instance-group-name"
+}
+
+variable "min_instances" {
+  description = "Minimum number of instances in the autoscaler"
+  default     = 1
+}
+
+variable "max_instances" {
+  description = "Maximum number of instances in the autoscaler"
+  default     = 10
+}
+
+
+
+variable "DNS_Zone_Name" {
+  type        = string
+  description = "Name of the existing Google Cloud DNS managed zone"
+}
+
+
+variable "Autoscaler_service_account_name" {
+  description = "Autoscaler Name of the Service Account"
+  type        = string
+}
+
+variable "Autoscaler_service_account_display_name" {
+  description = "Autoscaler Display Name of the Service Account"
+  type        = string
+}
+
+variable "it_disk_auto_delete" {
+  description = "Whether the boot disk should be automatically deleted when the instance is deleted."
+  type        = bool
+
+}
+
+variable "it_disk_boot" {
+  description = "Whether this is a boot disk."
+  type        = bool
+}
+
+variable "it_can_ip_forward" {
+  description = "Whether the instance can forward IP packets."
+  type        = bool
+}
+
+variable "it_template_name" {
+  description = "Name for the compute instance template."
+  type        = string
+}
+
+variable "it_template_description" {
+  description = "Description for the compute instance template."
+  type        = string
+}
+
+variable "it_scopes" {
+  description = "List of scopes for the instance. Valid values include 'cloud-platform', 'userinfo-email', 'compute-ro', and 'storage-ro'."
+  type        = list(string)
+}
+
+variable "health_check_interval_sec" {
+  description = "How often (in seconds) to send a health check. The default value is 60 seconds."
+  type        = number
+}
+
+variable "health_check_timeout_sec" {
+  description = "How long (in seconds) to wait before considering the operation as timeout. The default value is 5 seconds."
+  type        = number
+}
+
+variable "health_check_healthy_threshold" {
+  description = "The number of consecutive health check successes required before considering an unhealthy instance healthy. The default value is 2."
+  type        = number
+}
+
+variable "health_check_unhealthy_threshold" {
+  description = "The number of consecutive health check failures required before considering an instance unhealthy. The default value is 2."
+  type        = number
+}
+
+variable "health_check_name" {
+  description = "Name for the HTTP health check."
+  type        = string
+}
+
+variable "health_check_request_path" {
+  description = "The path to use for the HTTP health check request."
+  type        = string
+}
+
+variable "health_check_log_config_enable" {
+  description = "Whether logging should be enabled for the health check."
+  type        = bool
+}
+
+variable "instance_group_manager_name" {
+  description = "Name for the instance group manager."
+  type        = string
+}
+
+
+
+variable "base_instance_name" {
+  description = "Base name for the instances in the instance group."
+  type        = string
+  default     = "webapp"
+}
+
+variable "igm_base_instance_name" {
+  description = "Base name for the instances in the instance group."
+  type        = string
+}
+
+variable "igm_target_size" {
+  description = "The number of instances in the managed instance group."
+  type        = number
+}
+
+variable "igm_named_port_name" {
+  description = "The name of the named port."
+  type        = string
+}
+
+variable "ignm_named_port_port" {
+  description = "The port number of the named port."
+  type        = number
+}
+
+variable "igm_auto_healing_initial_delay_sec" {
+  description = "The initial delay (in seconds) for auto-healing."
+  type        = number
+}
+
+variable "autoscaler_name" {
+  description = "Name for the autoscaler."
+  type        = string
+}
+
+variable "autoscaler_max_replicas" {
+  description = "Maximum number of replicas for the autoscaler."
+  type        = number
+}
+
+variable "autoscaler_min_replicas" {
+  description = "Minimum number of replicas for the autoscaler."
+  type        = number
+}
+
+variable "autoscaler_cooldown_period" {
+  description = "Cooldown period (in seconds) for the autoscaler."
+  type        = number
+}
+
+variable "autoscaler_target" {
+  description = "Target CPU utilization for the autoscaler."
+  type        = number
+}
+
+variable "name_lb_ipv4_address" {
+  description = "The name attribute for the lb-ipv4-address resource"
+  type        = string
+}
+
+variable "rule_name" {
+  description = "The name attribute for the resource"
+  type        = string
+}
+
+variable "rule_ip_protocol" {
+  description = "The IP protocol for the resource"
+  type        = string
+}
+
+variable "rule_load_balancing_scheme" {
+  description = "The load balancing scheme for the resource"
+  type        = string
+}
+
+variable "rule_port_range" {
+  description = "The port range for the resource"
+  type        = string
+}
+
+variable "https_proxy_name" {
+  description = "The name attribute for the resource"
+  type        = string
+}
+
+variable "url_map_name" {
+  description = "The name attribute for the resource"
+  type        = string
+}
+
+variable "ssl_certificate_name" {
+  description = "The name attribute for the resource"
+  type        = string
+}
+
+variable "webapp_backend_name" {
+  description = "The name attribute for the resource"
+  type        = string
+}
+
+variable "webapp_backend_protocol" {
+  description = "The protocol for the resource"
+  type        = string
+}
+
+variable "webapp_backend_port_name" {
+  description = "The port name for the resource"
+  type        = string
+}
+
+variable "webapp_backend_load_balancing_scheme" {
+  description = "The load balancing scheme for the resource"
+  type        = string
+}
+
+variable "webapp_backend_timeout_sec" {
+  description = "The timeout in seconds for the resource"
+  type        = number
+}
+
+variable "webapp_backend_enable_cdn" {
+  description = "Flag to enable CDN for the resource"
+  type        = bool
+}
+
+variable "webapp_backend_connection_draining_timeout_sec" {
+  description = "The connection draining timeout in seconds for the resource"
+  type        = number
+}
+
+variable "webapp_backend_balancing_mode" {
+  description = "The balancing mode for the backend"
+  type        = string
+}
+
+variable "webapp_backend_capacity_scaler" {
+  description = "The capacity scaler for the backend"
+  type        = number
+}
+
+variable "peering_network_name" {
+  description = "The name of the network"
+  type        = string
+}
+
+variable "peering_auto_create_subnetworks" {
+  description = "Whether to enable auto-creation of subnetworks"
+  type        = bool
+}
+
+variable "webapp_route_priority" {
+  description = "The priority of the resource"
+  type        = number
+}
+
+variable "allow_webapplication_port_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "deny_ssh_port_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "private_ip_address_name" {
+  description = "The name of the IP address resource"
+  type        = string
+}
+
+variable "private_ip_address_purpose" {
+  description = "The purpose of the IP address resource"
+  type        = string
+}
+
+variable "private_ip_address_type" {
+  description = "The type of the IP address resource"
+  type        = string
+}
+
+variable "private_ip_prefix_length" {
+  description = "The prefix length of the IP address resource"
+  type        = number
+}
+
+variable "deny_ssh_ports" {
+  description = "List of allowed ports"
+  type        = list(string)
+}
+
+variable "deny_ssh_source_ranges" {
+  description = "List of allowed source IP ranges"
+  type        = list(string)
+}
+
+variable "vpc_connector_name" {
+  description = "The name of the VPC connector"
+  type        = string
+}
+
+variable "vpc_connector_ip_cidr_range" {
+  description = "The IP CIDR range for the VPC connector"
+  type        = string
+}
+
+variable "target_pools_name" {
+  description = "The name of the Target Pools."
+  type        = string
+}
+
+variable "locality_lb_policy_name" {
+  description = "The load balancing algorithm used within the scope of the locality."
+  type        = string
+}
+
+variable "compute_service_account_id" {
+  type        = string
+  description = "The ID of the service account"
+}
+
+variable "compute_service_display_name" {
+  type        = string
+  description = "The display name of the service account"
+}
+
+variable "allow_http_name" {
+  description = "Name of the resource. Provided by the client when the resource is created"
+  type        = string
+}
+
+variable "allow_http_direction" {
+  description = "Direction of traffic to which this firewall applies; default is INGRESS."
+  type        = string
+}
+
+variable "allow_http_protocol" {
+  type        = string
+  description = "The protocol to allow traffic for"
+}
+
+variable "allow_http_ports" {
+  type        = list(string)
+  description = "The list of ports to allow traffic for"
+}
+
+variable "allow_http_source_ranges" {
+  type        = list(string)
+  description = "The list of source IP ranges to allow traffic from"
+}
+
+variable "default_name" {
+  description = "Name of the resource. Provided by the client when the resource is created"
+  type        = string
+}
+
+variable "allow_lb_name" {
+  description = "Name of the resource. Provided by the client when the resource is created"
+  type        = string
+}
